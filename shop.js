05c51613891248901c5e7d2ee03cf7edc0624c7d
Azunyan Nyan Nyan <36520448+azunyan-nyan-nyan@users.noreply.github.com>
2020-10-03 01:41:30 -0400

    Add files via upload

---
+++ shop.js
@@ -0,0 +1,13 @@
+var shirts= document.getElementById("shirts");
+var shirt=shirts.childNodes;
+var opacity=0;
+var id = setInterval(fadein, 200);
+function fadein(x){
+    if(opacity==0){
+opacity+=0.1;
+document.getElementById("demo").innerHTML = opacity;
+shirt[x].style.opacity=opacity;
+    }
+}
+
+   
